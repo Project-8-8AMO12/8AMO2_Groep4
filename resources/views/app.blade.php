b0ef8975a6da3b7f74a0f1b85fe69243106e9631
ChristiaanvanHaasteren <1043218@mborijnland.nl>
2020-06-04 15:16:16 +0200

    Controllers aangemaakt
    De controller functies moeten nog gemaakt worden.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,20 +4,19 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Imkers Leiden</title>
+        <title>{{ env('APP_NAME') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
     </head>
     <body>
     <header>
         <!-- Top menu -->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="">
-                <h1 class="text-center">Imkers Leiden</h1>
+                <h1 class="text-center">{{ env('APP_NAME') }}</h1>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
